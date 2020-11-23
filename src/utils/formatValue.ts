56adad948f0b12d8da1efc2aa26889cde1b076cd
Gercino Luiz <gercino.neto.luiz@gmail.com>
2020-11-23 12:47:39 -0300

    latest

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,9 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+
+// Muito bom! Formatar valores
+  Intl.NumberFormat('pt-BR', {
+    style: "currency",
+    currency: "BRL"
+  }).format(value); // TODO
 
 export default formatValue;
